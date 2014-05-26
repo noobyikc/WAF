66957f533d12af133b8ca97de95031d81eaad501
slitherlink <akash.hada@yahoo.com>
2014-05-26 17:43:42 +0530

    Footer part added

---
+++ Footer.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+
+<title>World Auto Forum</title>
+        
+
+
+<meta charset="UTF-8">
+<title>Social Sharer</title>
+        
+<link rel="stylesheet" type="text/css" href="css/footer.css" />
+<link rel="stylesheet" href="http://www.eduardmayer.de/codepen-css-resources/webfonts.css"> 
+</head>
+
+<body>
+<footer>
+<div class='footer'>
+  <div class='footer-copy'>
+    <ul>
+      <li>
+         <i class='icon-eye-close'></i>
+        Jobs
+      </li>
+      <li>
+        <i class='icon-eye-close'></i>
+        Marketplace
+      </li>
+      <li>
+        <i class='icon-eye-close'></i>
+        Discussion Forum
+      </li>
+      <li>
+        <i class='icon-eye-close'></i>
+        Events
+      </li>       
+ <li>
+        <i class='icon-eye-close'></i>
+        Create Advert
+      </li>
+<li>
+        <i class='icon-eye-close'></i>
+        Create Page
+      </li>
+       <li>
+        <i class='icon-eye-close'></i>
+        Policy
+      </li>
+
+ <li>
+        <i class='icon-eye-close'></i>
+        About
+      </li>	    
+
+<li>
+<section class="pen">
+		<div class="more-button">
+			<div class="container">
+				<a href="#" class="share-btn icon-twitter"></a>
+				<a href="#" class="share-btn icon-facebook"></a>
+				<a href="#"  class="share-btn icon-google-plus-sign"></a>
+				<a href="#"  class="share-btn icon-pinterest"></a>
+				<a href="#"  class="share-btn icon-linkedin"></a>
+				<a href="#"  class="share-btn icon-github-alt"></a>
+			</div>
+		</div>
+	</section>
+ </li>   
+</ul> 
+  
+ </div>
+<div class='copyright'>(c) 2014 World Auto Forum, all rights reserved</div>
+</div>
+</footer>
+</body>
+</html>
