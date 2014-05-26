065d8da65eb93609d8aedd90e5433c839845c990
Inshu <mischeavious@gmail.com>
2014-05-26 14:55:40 +0530

    updated profile page

--- profile.php
+++ profile.php
@@ -7,58 +7,80 @@
 		<!-- CSS -->
             <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
             <link rel="stylesheet" type="text/css" href="css/profile.css">
+   			<link rel="stylesheet" type="text/css" href="css/search.css" />
+
+            <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap.js"></script>
+            <script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+            <script src="js/modernizr.custom.js"></script>
+         	<script src="js/classie.js"></script>
+			<script src="js/uisearch.js"></script>
+
             
 </head>
-
-<body>
-
-<!-- NAVBAR -->  
-    <div class="navbar-wrapper">
-      <div class="container">
-
-        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+ 	<body data-spy="scroll" data-target=".navbar navbar-inverse">
+   <!--=============================================Navigation============================================= --> 
+    	<div class="navbar-wrapper">
           <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">World Auto Forum</a>
-            </div>
-            <div class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="Dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
+            <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">World Auto Forum</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">Sign In</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li class="Dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                        <div class="main clearfix">
+						<!-- Optional columns for small components -->
+							<div class="column">
+								<div id="sb-search" class="sb-search">
+									<form>
+										<input class="sb-search-input" placeholder="Enter your search term..." type="text" value="" name="search" id="search">
+										<input class="sb-search-submit" type="submit" value="">
+										<span class="sb-icon-search"></span>
+									</form>
+								</div>
+							</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
           </div>
         </div>
+        <script>
+			new UISearch( document.getElementById( 'sb-search' ) );
+		</script>
+ 
+<div id="tabs">
+    <ul class="nav nav-tabs nav-justified">
+      <li><a href="#">Home</a></li>
+      <li class="active"><a href="#">Profile</a></li>
+      <li><a href="#">Messages</a></li>
+      <li><a href="#">Settings</a></li>
+      <li><a href="#">Help</a></li>
+    </ul>
+</div>
 
-      </div>
-    </div>
-
-<ul class="nav nav-tabs nav-justified">
-  <li><a href="#">Home</a></li>
-  <li class="active"><a href="#">Profile</a></li>
-  <li><a href="#">Messages</a></li>
-  <li><a href="#">Settings</a></li>
-  <li><a href="#">Help</a></li>
-</ul>
 
 <div class="panel panel-default" id="profile">
   <div class="panel-heading">Personal Details</div>
@@ -199,9 +221,7 @@
 </div>
   
 <div class="btn-group" id="button">
-  <button type="button" class="btn btn-primary">Edit Profile</button>
-  <button type="button" class="btn btn-primary">View as</button>
-  <button type="button" class="btn btn-primary">Save Profile</button>
+  <button type="button" class="btn btn-primary" style width="100%">Edit Profile Picture</button>
 </div>
 
   
