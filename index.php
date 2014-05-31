387e32bfaeea1a81bbc471d4b0855bf939e248fc
Manmeet Singh Oberoi <ms_oberoi@outlook.com>
2014-05-31 20:27:14 +0530

    Added loader on the start of page
    It will stay there till the images load up on the index page

--- index.php
+++ index.php
@@ -82,6 +82,18 @@
 	
 
   	<body data-spy="scroll" data-target=".navbar navbar-inverse">
+    
+   <!--=============================================Loader============================================= -->  
+		<script>
+			$(window).load(function() {
+			//$("#loader").animate({top: 700}, 2500);
+			  $("#loader").fadeOut([1000]);
+			});
+		</script>	
+		<div id="loader">
+        	<img id="image-loader"src="image/icons/gear.gif" alt="Loading..." />
+        </div>
+    
    <!--=============================================Navigation============================================= --> 
     	<div class="navbar-wrapper">
           <div class="container">
@@ -135,189 +147,180 @@
 		</script>
         
    <!--=============================================Login-Signup============================================= -->
-    <section>
-    	<div id="login-signup">
-			<a id="modal_trigger" href="#modal" class="btn">Login / Signup</a>
-			<div id="modal" class="popupContainer" style="display:none;">
-				<header class="popupHeader">
-					<span class="header_title">Enter World Auto Forum</span>
-					<span class="modal_close"><i>x</i></span>
-				</header>
-        
-		<section class="popupBody">
-			<!-- Social Login -->
-			<div class="social_login">
-				<div class="">
-					<a href="#" class="social_box fb">
-						<span class="icon"><i></i></span>
-						<span class="icon_title">Connect with Facebook</span>
-						
-					</a>
-
-					<a href="#" class="social_box google">
-						<span class="icon"><i class="fa fa-google-plus"></i></span>
-						<span class="icon_title">Connect with Google</span>
-					</a>
-				</div>
-
-				<div class="centeredText">
-					<span>Or use your Email address</span>
-				</div>
-
-				<div class="action_btns">
-					<div class="one_half"><a href="#" id="login_form" class="btn btn_red">Login</a></div>
-					<div class="one_half last"><a href="#" id="register_form" class="btn btn_red">Sign up</a></div>
-				</div>
+        <section>
+            <div id="login-signup">
+                <a id="modal_trigger" href="#modal" class="btn">Login / Signup</a>
+                <div id="modal" class="popupContainer" style="display:none;">
+                    <header class="popupHeader">
+                        <span class="header_title">Enter World Auto Forum</span>
+                        <span class="modal_close"><i>x</i></span>
+                    </header>
+            	<section class="popupBody">
+					<!-- Social Login -->
+					<div class="social_login">
+							<a href="#" class="social_box fb">
+								<span class="icon"><i></i></span>
+								<span class="icon_title">Continue with Facebook</span>
+							</a>
+                            <a href="#" class="social_box google">
+								<span class="icon"><i class="fa fa-google-plus"></i></span>
+								<span class="icon_title">Continue with Google</span>
+							</a>
+						<div class="centeredText">
+							<span>Or use your Email address</span>
+						</div>
+						<div class="action_btns">
+							<div class="one_half"><a href="#" id="login_form" class="btn btn_red">Login</a></div>
+							<div class="one_half last"><a href="#" id="register_form" class="btn btn_red">Sign up</a></div>
+						</div>
+					</div>
+					<!-- Username & Password Login form -->
+					<div class="user_login">
+						<form name="login" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+							<label>Email / Username</label>
+							<input type="text" name="email" />
+							<br />
+							<label>Password</label>
+							<input type="password" name="password" />
+							<br />
+							<div class="checkbox">
+								<input id="remember" type="checkbox" />
+								<label for="remember">Remember me on this computer</label>
+							</div>
+							<div class="action_btns">
+								<div class="one_half"><a href="#" class="btn back_btn">Back</a></div>
+								<div class="one_half last"><a class="btn btn_red" style="padding:1px 1px;"><input type="submit" class="btn btn_red" value="Login" id="login-reg_button" name="loginbtn" /></a></div>
+							</div>
+						</form>
+						<a href="#" class="forgot_password">Forgot password?</a>
+					</div>
+					<!-- Register Form -->
+					<div class="user_register">
+						<form name="signup" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" >
+							<label>Full Name</label>
+							<input type="text" name="name" />
+							<br />
+							<label>Email Address</label>
+							<input type="email" name="email" />
+							<br />
+							<label>Password</label>
+							<input type="password" name="password" />
+							<br />
+							<div class="checkbox">
+								<input id="send_updates" type="checkbox" name="updates" />
+								<label for="send_updates">Send me occasional email updates</label>
+							</div>
+							<div class="action_btns">
+								<div class="one_half"><a href="#" class="btn back_btn">Back</a></div>
+								<div class="one_half last"><a class="btn btn_red" style="padding:1px 1px;"><input name="registerbtn" class="btn btn_red" type="submit" id="login-reg_button" value="Register" /></a></div>
+							</div>
+						</form>
+					</div>
+				</section>
 			</div>
-
-			<!-- Username & Password Login form -->
-			<div class="user_login">
-				<form name="login" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-					<label>Email / Username</label>
-					<input type="text" name="email" />
-					<br />
-
-					<label>Password</label>
-					<input type="password" name="password" />
-					<br />
-
-					<div class="checkbox">
-						<input id="remember" type="checkbox" />
-						<label for="remember">Remember me on this computer</label>
-					</div>
-
-					<div class="action_btns">
-						<div class="one_half"><a href="#" class="btn back_btn">Back</a></div>
-						<div class="one_half last"><a class="btn btn_red"><input type="submit" class="btn btn_red" value="Login" id="login-reg_button" name="loginbtn" /></a></div>
-					</div>
-				</form>
-
-				<a href="#" class="forgot_password">Forgot password?</a>
-			</div>
-
-			<!-- Register Form -->
-			<div class="user_register">
-				<form name="signup" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" >
-					<label>Full Name</label>
-					<input type="text" name="name" />
-					<br />
-
-					<label>Email Address</label>
-					<input type="email" name="email" />
-					<br />
-
-					<label>Password</label>
-					<input type="password" name="password" />
-					<br />
-
-					<div class="checkbox">
-						<input id="send_updates" type="checkbox" name="updates" />
-						<label for="send_updates">Send me occasional email updates</label>
-					</div>
-
-					<div class="action_btns">
-						<div class="one_half"><a href="#" class="btn back_btn">Back</a></div>
-						<div class="one_half last"><a class="btn btn_red"><input name="registerbtn" class="btn btn_red" type="submit" id="login-reg_button" value="Register" /></a></div>
-					</div>
-				</form>
-			</div>
-		</section>
-	</div>
-</div>
-
-<script type="text/javascript">
-	$("#modal_trigger").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
-
-	$(function(){
-		// Calling Login Form
-		$("#login_form").click(function(){
-			$(".social_login").hide();
-			$(".user_login").show();
-			$(".header_title").text('Login');
-			return false;
-		});
-
-		// Calling Register Form
-		$("#register_form").click(function(){
-			$(".social_login").hide();
-			$(".user_register").show();
-			$(".header_title").text('Register');
-			return false;
-		});
-
-		// Going back to Social Forms
-		$(".back_btn").click(function(){
-			$(".user_login").hide();
-			$(".user_register").hide();
-			$(".social_login").show();
-			$(".header_title").text('Enter World Auto Forum');
-			return false;
-		});
-
-	})
-</script>
+		</div>
+		<script type="text/javascript">
+			$("#modal_trigger").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
+			$(function(){
+			// Calling Login Form
+				$("#login_form").click(function(){
+					$(".social_login").hide();
+					$(".user_login").show();
+					$(".header_title").text('Login');
+					return false;
+				});
+			// Calling Register Form
+				$("#register_form").click(function(){
+					$(".social_login").hide();
+					$(".user_register").show();
+					$(".header_title").text('Register');
+					return false;
+				});
+			// Going back to Social Forms
+				$(".back_btn").click(function(){
+					$(".user_login").hide();
+					$(".user_register").hide();
+					$(".social_login").show();
+					$(".header_title").text('Enter World Auto Forum');
+					return false;
+				});
+			})
+		</script>
     </section>
 	
-
   <!--=============================================Carousel============================================= -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="item active" id="carousel_image1">
-          
-          <div class="container">
-            <div class="carousel-caption">
-            	<h1>Join Us,</h1>
-                <p>Top Auto Makers & their People across 125 countries are on WAF!<br />Are You ?</p>
-            </div>
-          </div>
-        </div>
-        <div class="item" id="carousel_image2" >
-          <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide">
-          <div class="container">
-            <div class="carousel-caption">
-            	<h1>Buy stuff</h1>
-                <p>Auto Components & Parts Makers Love WAF!</p>
-            </div>
-          </div>
-        </div>
-        <div class="item" id="carousel_image3">
-          <img data-src="holder.js/900x500/auto/#555:#5a5a5a/">
-          <div class="container">
-            <div class="carousel-caption">
-            	<h1>Auto Dealers</h1>
-                <p>From around the world are on WAF!</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-    </div><!-- /.carousel -->
     
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    	<!-- Indicators -->
+		<ol class="carousel-indicators">
+        	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+	        <li data-target="#myCarousel" data-slide-to="1"></li>
+    	    <li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+        	<div class="item active" id="carousel_image1">
+          		<div class="container">
+		            <div class="carousel-caption">
+        		    	<h1>Join Us,</h1>
+                		<p>Top Auto Makers & their People across 125 countries are on WAF!<br />Are You ?</p>
+		            </div>
+        		</div>
+			</div>
+			<div class="item" id="carousel_image2" >
+				<div class="container">
+        	    	<div class="carousel-caption">
+		            	<h1>Buy stuff</h1>
+		                <p>Auto Components & Parts Makers Love WAF!</p>
+		            </div>
+        		</div>
+	        </div>
+    	    <div class="item" id="carousel_image3">
+				<div class="container">
+		            <div class="carousel-caption">
+		            	<h1>Auto Dealers</h1>
+		                <p>From around the world are on WAF!</p>
+		            </div>
+				</div>
+	        </div>
+    	</div>
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      	<a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div>
+    
+	<!--=============================================Heading============================================= -->
 
-
-
+	<div id="company"> 
+		W<font style="font-size:50px">ORLD</font>
+        A<font style="font-size:50px">UTO</font>
+        F<font style="font-size:50px">ORUM</font>
+    </div>
   
 
+	<!--=============================================Footer============================================= -->
+    
+    <!---->
 
-      <!-- FOOTER -->
-      <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-      </footer>
-
+    <div class="footer">
+		<table class="links">
+        	<tr>
+            	<td><a href="#">Sitemap</a></td>
+                <td><a href="#">Terms</a></td>
+                <td><a href="#">Privacy</a></td>
+                <td><a href="#">Jobs</a></td>
+            	<td><a href="#">Marketplace</a></td>
+                <td></td>
+            </tr>
+            <tr>
+            	<td><a href="#">Forum/Discussion</a></td>
+                <td><a href="#">Events</a></td>
+                <td><a href="#">Create Advt.</a></td>
+                <td><a href="#">Create Pages</a></td>
+                <td></td>
+        </table>
+        <div class="copyright">
+            <p>(c) 2014 World Auto Forum inc, all rights reserved</p>
+        </div>
+      </div>
     </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../../assets/js/docs.min.js"></script>
   </body>
 </html>
