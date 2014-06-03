cc191f79d706d6d9c4753e40829572dca7aec9eb
slitherlink <akash.hada@yahoo.com>
2014-06-03 14:37:05 +0530

    Progress Bar

---
+++ Progress Bar/PBar.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<link href='PBar.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="progress">
+  <div class="circle done">
+    <span class="label">1</span>
+    <span class="title">Sign-Up</span>
+  </div>
+  <span class="bar done"></span>
+  <div class="circle done">
+    <span class="label">2</span>
+    <span class="title">Your Domain</span>
+  </div>
+  <span class="bar half"></span>
+  <div class="circle active">
+    <span class="label">3</span>
+    <span class="title">Personal Details</span>
+  </div>
+ 
+  
+</div>
+<script src="jscript.js"></script>
+</body>
+</html>
