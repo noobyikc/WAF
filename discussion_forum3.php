6b2b7bb090477d5384bef7d3c27a8fac37ddd769
ishanmalhotra <ishanmalhotra100@gmail.com>
2014-06-23 21:40:53 +0530

    Discussion_forum

---
+++ discussion_forum3.php
@@ -0,0 +1,121 @@
+			
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="description" content="">
+            <meta name="author" content="">
+           
+            <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+            <link rel="shortcut icon" href="../favicon.ico">
+        
+            <title>World Auto Forum</title>
+        
+            <!-- CSS -->
+            <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+            <link rel="stylesheet" type="text/css" href="css/carousel.css" />
+            <link rel="stylesheet" type="text/css" href="css/index.css" />
+			<link rel="stylesheet" type="text/css" href="css/search.css" />
+			<link rel="stylesheet" type="text/css" href="css/default.css" />
+			<link rel="stylesheet" type="text/css" href="css/component.css" />
+			<link rel="stylesheet" type="text/css" href="css/component3.css" />
+			
+			
+			
+            <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap.js"></script>
+            <script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+            <script src="js/modernizr.custom.js"></script>
+         	<script src="js/classie.js"></script>
+			<script src="js/uisearch.js"></script>
+
+			<SCRIPT language=JavaScript>
+			function reload(form)
+			{
+				var val=form.cat.options[form.cat.options.selectedIndex].value;
+				self.location='discussion_forum2.php?cat=' + val ;
+			}
+
+			</script>
+
+          
+
+	</head>
+	   <body>
+      <!--=============================================Navigation============================================= --> 
+    	<div class="navbar-wrapper">
+          <div class="container">
+            <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">World Auto Forum</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">Sign In</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li class="Dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                        <div class="main clearfix">
+						<!-- Optional columns for small components -->
+							<div class="column">
+								<div id="sb-search" class="sb-search">
+									<form>
+										<input class="sb-search-input" placeholder="Enter your search term..." type="text" value="" name="search" id="search">
+										<input class="sb-search-submit" type="submit" value="">
+										<span class="sb-icon-search" id="search-img"><img src="image/icons/sprites.png"/></span>
+									</form>
+								</div>
+							</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+          </div>
+        </div>
+        <script>
+			new UISearch( document.getElementById( 'sb-search' ) );
+		</script>
+
+		<!--=============================================Form============================================= -->
+
+
+			<div id="form-main">
+				<div id="form-div">
+					<form class="form" id="form1">
+      
+						<p class="name">
+						<input name="Topic" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Topic" id="name" />
+						</p>
+      
+						
+						<p class="text">
+							<textarea name="info" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Comments"></textarea>
+						</p>
+      
+      
+						<div class="submit">
+							<input type="submit" value="Submit" id="button-blue"/>
+						<div class="ease"></div>
+						</div>
+					</form>
+				</div>
+			</div>	
