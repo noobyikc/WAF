70574bd264e774dc024d95ae14387d1c37206dd7
Manmeet Singh Oberoi <ms_oberoi@outlook.com>
2014-06-28 03:07:11 +0530

    Working on discussion forum template

--- discussion_forum.php
+++ discussion_forum.php
@@ -1,20 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-            
-            <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
-            <link rel="shortcut icon" href="../favicon.ico">
+    	<link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+        <link rel="shortcut icon" href="../favicon.ico">
         
-            <title>World Auto Forum</title>
+        <title>W.A.F Discussion Forum</title>
         
-           
-            <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-            <link rel="stylesheet" type="text/css" href="css/carousel.css" />
-            <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="css/discussion_forum.css" />
+        <!--<link rel="stylesheet" type="text/css" href="css/index.css" />-->
+        
 			<link rel="stylesheet" type="text/css" href="css/search.css" />
 			<link rel="stylesheet" type="text/css" href="css/default.css" />
 			<link rel="stylesheet" type="text/css" href="css/component2.css" />
 
-			<SCRIPT language=JavaScript>
+			<!--<SCRIPT language=JavaScript>
 			function reload(form)
 			{
 				var val=form.cat.options[form.cat.options.selectedIndex].value;
@@ -22,94 +22,105 @@
 			}
 
 			</script>
-			
+			-->
+            <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap.js"></script>
+            <script src="js/classie.js"></script>
+			<script src="js/uisearch.js"></script>
 
           
 
 	</head>
-	<body>
 	
-		  	<body data-spy="scroll" data-target=".navbar navbar-inverse">
-   <!--=============================================Navigation============================================= --> 
+  	<body data-spy="scroll" data-target=".navbar navbar-inverse">
+   		<!--=============================================Navigation============================================= --> 
     	<div class="navbar-wrapper">
-          <div class="container">
-            <div class="navbar navbar-inverse navbar-static-top" role="navigation">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">World Auto Forum</a>
+        	<div class="container">
+                <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        	<span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">World Auto Forum</a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#about">Sign In</a></li>
+                      		<li><a href="#contact">Contact</a></li>
+                        	<li class="Dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li class="divider"></li>
+                                    <li class="dropdown-header">Nav header</li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                        	</li>
+                        	<li>
+                                <div class="main clearfix">
+                                <!-- Optional columns for small components -->
+                                    <div class="column">
+                                        <div id="sb-search" class="sb-search">
+                                            <form>
+                                                <input class="sb-search-input" placeholder="Enter your search term..." type="text" value="" name="search" id="search">
+                                                <input class="sb-search-submit" type="submit" value="">
+                                                <span class="sb-icon-search" id="search-img"><img src="image/icons/sprites.png"/></span>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">Sign In</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li class="Dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li class="dropdown-header">Nav header</li>
-                                <li><a href="#">Separated link</a></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                        <div class="main clearfix">
-						<!-- Optional columns for small components -->
-							<div class="column">
-								<div id="sb-search" class="sb-search">
-									<form>
-										<input class="sb-search-input" placeholder="Enter your search term..." type="text" value="" name="search" id="search">
-										<input class="sb-search-submit" type="submit" value="">
-										<span class="sb-icon-search"></span>
-									</form>
-								</div>
-							</div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+              </div>
             </div>
-          </div>
+			<script>
+                new UISearch( document.getElementById( 'sb-search' ) );
+            </script>
+        <!--=============================================Side Pane============================================= --> 
+        <div class="sidepane">
         </div>
-        <script>
-			new UISearch( document.getElementById( 'sb-search' ) );
-		</script>
-
-		
-		<!--=============================================Timeline============================================= --> 
-   
-		
-		<div class="container">
-			<header class="clearfix">
-				<span></span>
-				<h1>  </h1><br><br><br>
-				
-			</header>	
-			<div class="main"><span><h1></h1></span>
+        <!--=============================================Discussions============================================= --> 
+        
+        <div class="heading1">
+            <h1>Discussions</h1>
+        </div>
+        <div id="sort-options">
+            <div class="tabs">
+                <ul class="tab-links">
+                    <li class="active" id="#tab1">New</li>
+                    <li id="#tab2">Top</li>
+                    <li id="#tab3">Rising</li>
+                    <li id="#tab4">Promoted</li>
+                    <li id="#tab5">F.A.Q</li>
+                </ul>
+            </div>
+         	<div class="holder1">
 				<ul class="cbp_tmtimeline">
 					<li>
-						
-						<div class="cbp_tmicon cbp_tmicon-phone"></div>
+						<div class="cbp_tmicon ">
+                        	<!-- Add upvotes downvotes number here this comes from database dynamically-->
+                        </div>
 						<div class="cbp_tmlabel">
 							<div class="share">
 								<a href="#"><image src="image/logo/facebook-icon.png" style="height:30px;width:30px;"></a>
 								<a href="#"><image src="image/logo/gplus.png" style="height:30px;width:30px;"></a>
-								<a href="#"><image src="image/logo/linkedln.png" style="height:30px;width:30px;"></a>
-								
+								<a href="#"><image src="image/logo/linkedln.png" style="height:30px;width:30px;"></a>				
 							</div>
-							<h2><a href="#">How do i log in??</a></h2>
-							<p>I am having problem loging in WAF.So plzzz help me and let me log in.</p>
-							
-						</div>
-						<textarea placeholder="Write Your Answer" rows="3" name="comment[text]" id="comment_text" cols="40" class="ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true"></textarea>
-						<SCRIPT language=JavaScript>
+                            <h4><a href="#">How do i log in??</a></h4>
+                            <p>I am having problem loging in WAF.So plzzz help me and let me log in.</p>
+                        </div>
+                    </li>
+             
+						<!--<textarea placeholder="Write Your Answer" rows="3" name="comment[text]" id="comment_text" cols="40" class="ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true"></textarea>-->
+						<!--<SCRIPT language=JavaScript>
 							var textarea = document.querySelector('textarea');
 
 							textarea.addEventListener('keydown', autosize);
@@ -124,7 +135,7 @@
 								},0);
 							}
 
-						</script>
+						</script>-->
 					</li>
 					<li>
 						
@@ -240,5 +251,7 @@
 					</li>
 					
 				</ul>
-			</div>
-		</div>
+			</div>	
+            </div>
+	</body>
+</html>
