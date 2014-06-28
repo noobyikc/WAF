8bd05a8bafd07d5f3cf0f4ba1aa992154e70337b
Inshu <mischeavious@gmail.com>
2014-06-28 16:56:33 +0530

    small updates

--- index.php
+++ index.php
@@ -31,62 +31,6 @@
             <script type="text/javascript" src="js/jssor.slider.js"></script>
 </head>
 
-<!-----slider script--->
-<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
-    <!-- use jssor.slider.mini.js (39KB) or jssor.sliderc.mini.js (31KB, with caption, no slideshow) or jssor.sliders.mini.js (26KB, no caption, no slideshow) instead for release -->
-    <!-- jssor.slider.mini.js = jssor.sliderc.mini.js = jssor.sliders.mini.js = (jssor.core.js + jssor.utils.js + jssor.slider.js) -->
-    <script type="text/javascript" src="../js/jssor.core.js"></script>
-    <script type="text/javascript" src="../js/jssor.utils.js"></script>
-    <script type="text/javascript" src="../js/jssor.slider.js"></script>
-    <script>
-        jQuery(document).ready(function ($) {
-            var options = {
-                $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-                $AutoPlaySteps: 1,                                  //[Optional] Steps to go for each navigation request (this options applys only when slideshow disabled), the default value is 1
-                $AutoPlayInterval: 0,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
-                $PauseOnHover: 1,                               //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, default value is 1
-
-                $ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
-                $SlideEasing: $JssorEasing$.$EaseLinear,          //[Optional] Specifies easing for right to left animation, default value is $JssorEasing$.$EaseOutQuad
-                $SlideDuration: 3000,                                //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
-                $MinDragOffsetToSlide: 20,                          //[Optional] Minimum drag offset to trigger slide , default value is 20
-                $SlideWidth: 140,                                   //[Optional] Width of every slide in pixels, default value is width of 'slides' container
-                //$SlideHeight: 100,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
-                $SlideSpacing: 0, 					                //[Optional] Space between each slide in pixels, default value is 0
-                $DisplayPieces: 7,                                  //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
-                $ParkingPosition: 0,                              //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
-                $UISearchMode: 1,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
-                $PlayOrientation: 1,                                //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, default value is 1
-                $DragOrientation: 1                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
-            };
-
-            var jssor_slider1 = new $JssorSlider$("slider1_container", options);
-
-            //responsive code begin
-            //you can remove responsive code if you don't want the slider scales while window resizes
-            function ScaleSlider() {
-                var bodyWidth = document.body.clientWidth;
-                if (bodyWidth)
-                    jssor_slider1.$SetScaleWidth(Math.min(bodyWidth, 980));
-                else
-                    window.setTimeout(ScaleSlider, 30);
-            }
-
-            ScaleSlider();
-
-            if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
-                $(window).bind('resize', ScaleSlider);
-            }
-
-
-            //if (navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
-            //    $(window).bind("orientationchange", ScaleSlider);
-            //}
-            //responsive code end
-        });
-    </script>
-<!----slider--->
-
 
 <?php
  		include('includes/connection.php');
@@ -164,6 +108,62 @@
 }
 ?>
 		   
+<!-----slider script--->
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <!-- use jssor.slider.mini.js (39KB) or jssor.sliderc.mini.js (31KB, with caption, no slideshow) or jssor.sliders.mini.js (26KB, no caption, no slideshow) instead for release -->
+    <!-- jssor.slider.mini.js = jssor.sliderc.mini.js = jssor.sliders.mini.js = (jssor.core.js + jssor.utils.js + jssor.slider.js) -->
+    <script type="text/javascript" src="js/jssor.core.js"></script>
+    <script type="text/javascript" src="js/jssor.utils.js"></script>
+    <script type="text/javascript" src="js/jssor.slider.js"></script>
+    <script>
+        jQuery(document).ready(function ($) {
+            var options = {
+                $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+                $AutoPlaySteps: 1,                                  //[Optional] Steps to go for each navigation request (this options applys only when slideshow disabled), the default value is 1
+                $AutoPlayInterval: 0,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+                $PauseOnHover: 1,                               //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, default value is 1
+
+                $ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
+                $SlideEasing: $JssorEasing$.$EaseLinear,          //[Optional] Specifies easing for right to left animation, default value is $JssorEasing$.$EaseOutQuad
+                $SlideDuration: 3000,                                //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+                $MinDragOffsetToSlide: 20,                          //[Optional] Minimum drag offset to trigger slide , default value is 20
+                $SlideWidth: 140,                                   //[Optional] Width of every slide in pixels, default value is width of 'slides' container
+                //$SlideHeight: 100,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
+                $SlideSpacing: 0, 					                //[Optional] Space between each slide in pixels, default value is 0
+                $DisplayPieces: 7,                                  //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
+                $ParkingPosition: 0,                              //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
+                $UISearchMode: 1,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
+                $PlayOrientation: 1,                                //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, default value is 1
+                $DragOrientation: 1                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+            };
+
+            var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizes
+            function ScaleSlider() {
+                var bodyWidth = document.body.clientWidth;
+                if (bodyWidth)
+                    jssor_slider1.$SetScaleWidth(Math.min(bodyWidth, 980));
+                else
+                    window.setTimeout(ScaleSlider, 30);
+            }
+
+            ScaleSlider();
+
+            if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
+                $(window).bind('resize', ScaleSlider);
+            }
+
+
+            //if (navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
+            //    $(window).bind("orientationchange", ScaleSlider);
+            //}
+            //responsive code end
+        });
+    </script>
+<!----slider--->
+
           
 
 	
@@ -507,44 +507,47 @@
             	<div class="details10">
                 	<h1>People from these countries are here. Are you?</h1>
                 </div>
+                
                 <!--  INSERT SCROLLER HERE-->
                 <!-- Jssor Slider Begin -->
     <!-- You can move inline styles (except 'top', 'left', 'width' and 'height') to css file or css block. -->
-    <div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 980px; height: 100px; overflow: hidden; ">
+    <div id="slider1_container" style="position: relative; top: 20px; left: 0px; width: 980px; height: 100px; overflow: hidden; ">
 
-        <!-- Loading Screen -->
-        <div u="loading" style="position: absolute; top: 0px; left: 0px;">
-            <div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block;
-                background-color: #000; top: 0px; left: 0px;width: 100%;height:100%;">
-            </div>
-            <div style="position: absolute; display: block; background: url(../img/loading.gif) no-repeat center center;
-                top: 0px; left: 0px;width: 100%;height:100%;">
-            </div>
-        </div>
 
         <!-- Slides Container -->
-        <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 980px; height: 100px; overflow: hidden;">
-            <div><img u="image" alt="amazon" src="../img/logo/amazon.jpg" /></div>
-            <div><img u="image" alt="android" src="../img/logo/android.jpg" /></div>
-            <div><img u="image" alt="bitly" src="../img/logo/bitly.jpg" /></div>
-            <div><img u="image" alt="blogger" src="../img/logo/blogger.jpg" /></div>
-            <div><img u="image" alt="dnn" src="../img/logo/dnn.jpg" /></div>
-            <div><img u="image" alt="drupal" src="../img/logo/drupal.jpg" /></div>
-            <div><img u="image" alt="ebay" src="../img/logo/ebay.jpg" /></div>
-            <div><img u="image" alt="facebook" src="../img/logo/facebook.jpg" /></div>
-            <div><img u="image" alt="google" src="../img/logo/google.jpg" /></div>
-            <div><img u="image" alt="ibm" src="../img/logo/ibm.jpg" /></div>
-            <div><img u="image" alt="ios" src="../img/logo/ios.jpg" /></div>
-            <div><img u="image" alt="joomla" src="../img/logo/joomla.jpg" /></div>
-            <div><img u="image" alt="linkedin" src="../img/logo/linkedin.jpg" /></div>
-            <div><img u="image" alt="mac" src="../img/logo/mac.jpg" /></div>
-            <div><img u="image" alt="magento" src="../img/logo/magento.jpg" /></div>
-            <div><img u="image" alt="pinterest" src="../img/logo/pinterest.jpg" /></div>
-            <div><img u="image" alt="samsung" src="../img/logo/samsung.jpg" /></div>
-            <div><img u="image" alt="twitter" src="../img/logo/twitter.jpg" /></div>
-            <div><img u="image" alt="windows" src="../img/logo/windows.jpg" /></div>
-            <div><img u="image" alt="wordpress" src="../img/logo/wordpress.jpg" /></div>
-            <div><img u="image" alt="youtube" src="../img/logo/youtube.jpg" /></div>
+        <div u="slides" style="cursor: move; position: absolute; left: 20px; top: 0px; width: 980px; height: 100px; overflow: hidden;">
+            <li><img class="img_thumb thumb" alt="audi" src="image/logo/New folder/logos/Audi_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="goodyear" src="image/logo/New folder/logos/545px-Goodyear_logo.svg.png" /></li>
+                <li><img class="img_thumb thumb" alt="siemens" src="image/logo/New folder/logos/181806_316_siemens.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="amtek" src="image/logo/New folder/logos/amtek_logo.png" /></li>
+                <li><img class="img_thumb thumb" alt="bosch" src="image/logo/New folder/logos/bosch_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="bajaj" src="image/logo/New folder/logos/bajaj_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="bmw bikes logo" src="image/logo/New folder/logos/bmw bikes_logo.JPG" /></li>
+                <li><img class="img_thumb thumb" alt="fiat" src="image/logo/New folder/logos/fiat_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="ford" src="image/logo/New folder/logos/ford_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="fuso" src="image/logo/New folder/logos/fuso_logo.JPG" /></li>
+                <li><img class="img_thumb thumb" alt="gm" src="image/logo/New folder/logos/gm_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="castrol" src="image/logo/New folder/logos/Castrol_Logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="caparo" src="image/logo/New folder/logos/caparo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="harley davidson" src="image/logo/New folder/logos/Harley-Davidson-Logo-300x225.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="visteon" src="image/logo/New folder/logos/visteon_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="tvs" src="image/logo/New folder/logos/tvs.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="bmw logo" src="image/logo/New folder/logos/bmw_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="maruti suzuki" src="image/logo/New folder/logos/Maruti-Suzuki-new-logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="hero" src="image/logo/New folder/logos/hero_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="hyundai" src="image/logo/New folder/logos/hyundai_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="isuzu" src="image/logo/New folder/logos/Isuzu-Logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="mazda" src="image/logo/New folder/logos/mazda_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="mercedes" src="image/logo/New folder/logos/merc_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="mitsubushi" src="image/logo/New folder/logos/mitsubishi_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="nissan" src="image/logo/New folder/logos/nissan_Logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="renault" src="image/logo/New folder/logos/renault_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="suzuki" src="image/logo/New folder/logos/suzuki_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="tata" src="image/logo/New folder/logos/tata_logo.jpg" /></li>
+               	<li><img class="img_thumb thumb" alt="toyota" src="image/logo/New folder/logos/Toyota_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="volvo" src="image/logo/New folder/logos/volvo cars_logo.jpg" /></li>
+				<li><img class="img_thumb thumb" alt="volkswagen" src="image/logo/New folder/logos/VW_logo.jpg" /></li>
+                <li><img class="img_thumb thumb" alt="yamaha" src="image/logo/New folder/logos/yamaha_logo.jpg" /></li>
         </div>
         
     </div>
