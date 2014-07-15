ac75dad746cffceec2c2134d0209d6ea7f1a6e4e
Manmeet Singh Oberoi <ms_oberoi@outlook.com>
2014-07-16 02:57:43 +0530

    added mailing functionality

--- index.php
+++ index.php
@@ -39,7 +39,37 @@
 	    include('includes/variables.php');
  
 ?>
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
 
+	$mail = new PHPMailer;	
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'smtp.gmail.com';						  // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	$mail->Username = 'noreply.waf@gmail.com';            // SMTP username
+	$mail->Password = 'waf@anuj12345,.';                           // SMTP password
+	$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+	$mail->From = 'noreply.waf@gmail.com';
+	$mail->FromName = 'WAF Mailer';
+	$mail->addAddress('aakash11003@iiitd.ac.in', 'Soloman Grundy');     // Add a recipient
+	//$mail->addAddress('ellen@example.com');               // Name is optional
+	$mail->addReplyTo('aakash11003@iiitd.ac.in', 'Napa, the invincible');
+	//$mail->addCC('cc@example.com');
+	//$mail->addBCC('bcc@example.com');
+	$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+	//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+	//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+	$mail->isHTML(true);                                  // Set email format to HTML
+	$mail->Subject = 'Test Mail';
+	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	if(!$mail->send()) {
+		echo 'Message could not be sent.';
+		echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+		echo 'Message has been sent';
+	}
+?>
 <?php
 		if(isset($_COOKIE['cookmail']) && isset($_COOKIE['cookpass']))
 		{
